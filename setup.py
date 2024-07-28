55fb2f4e472aebaaa3a574e367e47ca47d1094f4
Yuvraj-1107-ML <yuvrajdawande373@gmail.com>
2024-07-28 19:19:03 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import find_packages,setup
-
-setup(  name="ML project",
+from typing import List
+hypen_dot='-e .'
+def get_requirement(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("/n"," ") for req in requirements ]
+    if hypen_dot in requirements:
+        requirements.remove(hypen_dot)
+    return requirements
+         
+setup( 
+  name="ML project",
   version="0.0.1",
   author="Yuvraj Dawande",
-  author_email="yuvrajdawande373@gmail.com",
+ author_email="yuvrajdawande373@gmail.com",
  packages=find_packages(),
- install_requires=["numpy","pandas","matplotlib","seaborn"])
+ install_requires=get_requirement('requirements.txt'))
 
