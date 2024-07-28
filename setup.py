f87e8cd0b228774fb1c3ef797a2d01cb25d5ec32
Yuvraj-1107-ML <yuvrajdawande373@gmail.com>
2024-07-28 18:27:35 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(  name="ML project",
+  version="0.0.1",
+  author="Yuvraj Dawande",
+  author_email="yuvrajdawande373@gmail.com",
+ packages=find_packages(),
+ install_requires=["numpy","pandas","matplotlib","seaborn"])
+
